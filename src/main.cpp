57fb37e1def87325aff8117ddb8c4611d20457b9
Dean Liu <windsurf_dean@yahoo.com>
2018-06-20 17:13:24 -0700

    Make WPTs as the car's direct next points, just to test it out...

--- src/main.cpp
+++ src/main.cpp
@@ -36,134 +36,135 @@ string hasData(string s) {
   return "";
 }
 
-double distance(double x1, double y1, double x2, double y2)
-{
-	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-}
-int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
-{
+//double distance(double x1, double y1, double x2, double y2)
+//{
+//	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+//}
 
-	double closestLen = 100000; //large number
-	int closestWaypoint = 0;
+//int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
+//{
+//
+//	double closestLen = 100000; //large number
+//	int closestWaypoint = 0;
+//
+//	for(int i = 0; i < maps_x.size(); i++)
+//	{
+//		double map_x = maps_x[i];
+//		double map_y = maps_y[i];
+//		double dist = distance(x,y,map_x,map_y);
+//		if(dist < closestLen)
+//		{
+//			closestLen = dist;
+//			closestWaypoint = i;
+//		}
+//
+//	}
+//
+//	return closestWaypoint;
+//
+//}
 
-	for(int i = 0; i < maps_x.size(); i++)
-	{
-		double map_x = maps_x[i];
-		double map_y = maps_y[i];
-		double dist = distance(x,y,map_x,map_y);
-		if(dist < closestLen)
-		{
-			closestLen = dist;
-			closestWaypoint = i;
-		}
-
-	}
-
-	return closestWaypoint;
-
-}
-
-int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-
-	int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
-
-	double map_x = maps_x[closestWaypoint];
-	double map_y = maps_y[closestWaypoint];
-
-	double heading = atan2((map_y-y),(map_x-x));
-
-	double angle = fabs(theta-heading);
-  angle = min(2*pi() - angle, angle);
-
-  if(angle > pi()/4)
-  {
-    closestWaypoint++;
-  if (closestWaypoint == maps_x.size())
-  {
-    closestWaypoint = 0;
-  }
-  }
-
-  return closestWaypoint;
-}
+//int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+//{
+//
+//	int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
+//
+//	double map_x = maps_x[closestWaypoint];
+//	double map_y = maps_y[closestWaypoint];
+//
+//	double heading = atan2((map_y-y),(map_x-x));
+//
+//	double angle = fabs(theta-heading);
+//  angle = min(2*pi() - angle, angle);
+//
+//  if(angle > pi()/4)
+//  {
+//    closestWaypoint++;
+//  if (closestWaypoint == maps_x.size())
+//  {
+//    closestWaypoint = 0;
+//  }
+//  }
+//
+//  return closestWaypoint;
+//}
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
-vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
-
-	int prev_wp;
-	prev_wp = next_wp-1;
-	if(next_wp == 0)
-	{
-		prev_wp  = maps_x.size()-1;
-	}
-
-	double n_x = maps_x[next_wp]-maps_x[prev_wp];
-	double n_y = maps_y[next_wp]-maps_y[prev_wp];
-	double x_x = x - maps_x[prev_wp];
-	double x_y = y - maps_y[prev_wp];
-
-	// find the projection of x onto n
-	double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
-	double proj_x = proj_norm*n_x;
-	double proj_y = proj_norm*n_y;
-
-	double frenet_d = distance(x_x,x_y,proj_x,proj_y);
-
-	//see if d value is positive or negative by comparing it to a center point
-
-	double center_x = 1000-maps_x[prev_wp];
-	double center_y = 2000-maps_y[prev_wp];
-	double centerToPos = distance(center_x,center_y,x_x,x_y);
-	double centerToRef = distance(center_x,center_y,proj_x,proj_y);
-
-	if(centerToPos <= centerToRef)
-	{
-		frenet_d *= -1;
-	}
-
-	// calculate s value
-	double frenet_s = 0;
-	for(int i = 0; i < prev_wp; i++)
-	{
-		frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
-	}
-
-	frenet_s += distance(0,0,proj_x,proj_y);
-
-	return {frenet_s,frenet_d};
-
-}
+//vector<double> getFrenet(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
+//{
+//	int next_wp = NextWaypoint(x,y, theta, maps_x,maps_y);
+//
+//	int prev_wp;
+//	prev_wp = next_wp-1;
+//	if(next_wp == 0)
+//	{
+//		prev_wp  = maps_x.size()-1;
+//	}
+//
+//	double n_x = maps_x[next_wp]-maps_x[prev_wp];
+//	double n_y = maps_y[next_wp]-maps_y[prev_wp];
+//	double x_x = x - maps_x[prev_wp];
+//	double x_y = y - maps_y[prev_wp];
+//
+//	// find the projection of x onto n
+//	double proj_norm = (x_x*n_x+x_y*n_y)/(n_x*n_x+n_y*n_y);
+//	double proj_x = proj_norm*n_x;
+//	double proj_y = proj_norm*n_y;
+//
+//	double frenet_d = distance(x_x,x_y,proj_x,proj_y);
+//
+//	//see if d value is positive or negative by comparing it to a center point
+//
+//	double center_x = 1000-maps_x[prev_wp];
+//	double center_y = 2000-maps_y[prev_wp];
+//	double centerToPos = distance(center_x,center_y,x_x,x_y);
+//	double centerToRef = distance(center_x,center_y,proj_x,proj_y);
+//
+//	if(centerToPos <= centerToRef)
+//	{
+//		frenet_d *= -1;
+//	}
+//
+//	// calculate s value
+//	double frenet_s = 0;
+//	for(int i = 0; i < prev_wp; i++)
+//	{
+//		frenet_s += distance(maps_x[i],maps_y[i],maps_x[i+1],maps_y[i+1]);
+//	}
+//
+//	frenet_s += distance(0,0,proj_x,proj_y);
+//
+//	return {frenet_s,frenet_d};
+//
+//}
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
-vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
-{
-	int prev_wp = -1;
-
-	while(s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1) ))
-	{
-		prev_wp++;
-	}
-
-	int wp2 = (prev_wp+1)%maps_x.size();
-
-	double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),(maps_x[wp2]-maps_x[prev_wp]));
-	// the x,y,s along the segment
-	double seg_s = (s-maps_s[prev_wp]);
-
-	double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
-	double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
-
-	double perp_heading = heading-pi()/2;
-
-	double x = seg_x + d*cos(perp_heading);
-	double y = seg_y + d*sin(perp_heading);
-
-	return {x,y};
-
-}
+//vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
+//{
+//	int prev_wp = -1;
+//
+//	while(s > maps_s[prev_wp+1] && (prev_wp < (int)(maps_s.size()-1) ))
+//	{
+//		prev_wp++;
+//	}
+//
+//	int wp2 = (prev_wp+1)%maps_x.size();
+//
+//	double heading = atan2((maps_y[wp2]-maps_y[prev_wp]),(maps_x[wp2]-maps_x[prev_wp]));
+//	// the x,y,s along the segment
+//	double seg_s = (s-maps_s[prev_wp]);
+//
+//	double seg_x = maps_x[prev_wp]+seg_s*cos(heading);
+//	double seg_y = maps_y[prev_wp]+seg_s*sin(heading);
+//
+//	double perp_heading = heading-pi()/2;
+//
+//	double x = seg_x + d*cos(perp_heading);
+//	double y = seg_y + d*sin(perp_heading);
+//
+//	return {x,y};
+//
+//}
 
 int main() {
   uWS::Hub h;
@@ -237,11 +238,11 @@ int main() {
           // j[1] is the data JSON object
 
         	// Main car's localization Data
-          double car_x = j[1]["x"];
-          double car_y = j[1]["y"];
+          double car_x = j[1]["x"];     // [m]
+          double car_y = j[1]["y"];     // [m]
           double car_s = j[1]["s"];
           double car_d = j[1]["d"];
-          double car_yaw = j[1]["yaw"];
+          double car_yaw = j[1]["yaw"]; // [deg]
           double car_speed = j[1]["speed"];
 
           // Previous path data given to the Planner
@@ -259,13 +260,34 @@ int main() {
           cout << "dt " << dt << ", Hz " << 1/dt << endl;
 
 
-       // ---- Below are OUTPUTS ----
+       // ======== main algorithm goes here ==========
 
-
+          // containers for algorithm output
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
+          int num_wpts = map_waypoints_x.size();
 
+          int wpt_idx = NextWaypoint(car_x, car_y, deg2rad(car_yaw), map_waypoints_x, map_waypoints_y);
+
+          double next_wpt_x = map_waypoints_x[wpt_idx];
+          double next_wpt_y = map_waypoints_y[wpt_idx];
+
+          // fill 1st pt with car's current position
+          next_x_vals.push_back(car_x);
+          next_y_vals.push_back(car_y);
+
+          // fill 10 more with straight up wpts... (car's gonna FLY!!!)
+          for (int i = 0; i < 10; i++) {
+            wpt_idx = wpt_idx % num_wpts;
+            next_x_vals.push_back(map_waypoints_x[wpt_idx]);
+            next_y_vals.push_back(map_waypoints_y[wpt_idx]);
+            wpt_idx++;
+          }
+
+
+
+       // ---- Below are OUTPUTS ----
 
           json msgJson;
 
